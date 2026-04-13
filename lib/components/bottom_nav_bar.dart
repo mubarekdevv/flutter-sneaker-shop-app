1454a115f2b8e39981ea933aac827eac9de57e5f
Mubarek Jemal <mubarekj71@gmail.com>
2026-04-13 14:46:04 +0300

    perfect navigation from shop page ot cart page and vicevercal by Google buttons in bottm nav bar

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MyBottomNavBar extends StatelessWidget {
-  const MyBottomNavBar({super.key});
+
+  void Function(int)? onTabChangeu;
+  MyBottomNavBar(
+    {super.key,
+    required this.onTabChangeu
+    });
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +20,17 @@ class MyBottomNavBar extends StatelessWidget {
         tabActiveBorder: Border.all(color: Colors.white),
         tabBackgroundColor: Colors.grey.shade100,
         mainAxisAlignment: MainAxisAlignment.center,
+
+/*The job of the ff onTanChange property is to pass the selected tab number (0 for Shop, 1 for Cart) from the custom navigation bar back up to the HomePage.
+🔍 Breakdown
+The Outer onTabChange: This belongs to the Google Nav Bar (GNav) package. It triggers automatically and gives you a value (the index number of the tab clicked).
+The Inner onTabChange!(value): This is the custom function you created at the top of your MyBottomNavBar class.
+The Exclamation Mark (!): This tells Flutter that you promise this function is not null and is safe to run.
+In short, it acts like a bridge. It catches the click from the Google Nav Bar package and throws it up to your HomePage so it can swap the body display.*/
+        
+        onTabChange: (value) {
+          onTabChangeu!(value);
+        },
         tabs: [
         GButton(
           
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:ecommerce_app_ui/components/bottom_nav_bar.dart';
+import 'package:ecommerce_app_ui/pages/cart_page.dart';
+import 'package:ecommerce_app_ui/pages/shop_page.dart';
 import 'package:flutter/material.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,13 +10,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //this selected index is to control the bottom nav bar or we need to have an intiger to keep track of the selected index
+  int currentSelectedIndex = 0;
+  //also we need a method to update the selected index, when the user taps on the bottm bar and zero for the home and 1 for the cart it just givs it to the method and the method assigns it to the selectedindex so this means the method updates our selected index
+  updateCurrentSelectedIndex(index){
+    setState(() {
+  currentSelectedIndex = index;
+});
+}
+
+  //pages to display
+  final List<Widget> pages = [
+    //shop page
+    ShopPage(),
+    //cart page
+    CartPage()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       bottomNavigationBar: MyBottomNavBar(
-        
+        //the ff is to navigating the bottom bar
+        onTabChangeu: (index) => updateCurrentSelectedIndex(index),
       ),
+      body: pages[currentSelectedIndex],
     );
   }
 }
