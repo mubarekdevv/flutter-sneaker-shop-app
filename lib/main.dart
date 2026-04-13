de2f51b434175be448b58eeb19e83ae1de336e9d
Mubarek Jemal <mubarekj71@gmail.com>
2026-04-13 09:35:42 +0300

    intro page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app_ui/pages/intro_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: IntroPage(),
 
     );
   }
