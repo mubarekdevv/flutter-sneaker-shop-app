4ecdc6672230d183a08b513ddf7b222c528ac059
Mubarek Jemal <mubarekj71@gmail.com>
2026-04-13 09:45:02 +0300

    logo and title added

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -5,6 +5,29 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              //logo
+              padding: const EdgeInsets.only(top: 100),
+              child: Image.asset(
+                'assets/images/nikelogo.png',
+                height: 240,
+              ),
+            ),
+            //title
+            Text('Just do it!',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 10,
+            ),
+            
+          ],
+        ),
+      ),
+    );
   }
 }
