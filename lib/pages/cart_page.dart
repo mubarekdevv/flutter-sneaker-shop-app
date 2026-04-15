2bdf9e3006d436891c88917d2bfd789db4497053
Mubarek Jemal <mubarekj71@gmail.com>
2026-04-15 06:59:00 +0300

    Cart page implemented and tap to add in our cart working and displayed on cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,17 +1,35 @@
-import 'package:flutter/material.dart';
+import 'package:ecommerce_app_ui/components/cart_item.dart';
+import 'package:ecommerce_app_ui/models/cart.dart';
 
-class CartPage extends StatefulWidget {
+import '../models/shoe.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
-  State<CartPage> createState() => _CartPageState();
-}
-
-class _CartPageState extends State<CartPage> {
-  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Cart'),
-    );
+    return Consumer<Cart>(builder: (context, value, child) => Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18),
+      child: Column(
+        children: [
+          Text('My Cart', style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold
+          ),),
+          SizedBox(height: 10,),
+
+          Expanded(child: ListView.builder(
+            itemCount: value.getUserCart().length,
+            itemBuilder: (context, index) {
+            //get individual shoe
+            Shoe indiviualShoe = value.getUserCart()[index];
+            //return the cart item
+            return CartItem(shoe: indiviualShoe);
+          },))
+        ],
+      ),
+    ),);
   }
 }
