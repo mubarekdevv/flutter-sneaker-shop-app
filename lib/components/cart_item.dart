7cd1e75fb852e2406ae3b18e8d47b79afe5398a0
Mubarek Jemal <mubarekj71@gmail.com>
2026-04-15 07:09:04 +0300

    list tiles in the cart decorated

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -15,10 +15,18 @@ class CartItem extends StatefulWidget {
 class _CartItemState extends State<CartItem> {
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Image.asset(widget.shoe.imagePath),
-      title: Text(widget.shoe.name),
-      subtitle: Text(widget.shoe.price),
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        
+      ),
+      child: ListTile(
+        leading: Image.asset(widget.shoe.imagePath),
+        title: Text(widget.shoe.name),
+        subtitle: Text('\$' + widget.shoe.price),
+      ),
     );
   }
 }
