e421ba36aa04210245a9302a77722fb9a672df5d
Mubarek Jemal <mubarekj71@gmail.com>
2026-04-25 12:00:00 +0300

    Backdated commit to fill gap

--- lib/models/shoe.dart
+++ lib/models/shoe.dart
@@ -2,15 +2,13 @@ class Shoe {
   final String name;
   final String price;
   final String imagePath;
-  final String description;
-  int quantity;
+  
 
   Shoe(
     {required this.name,
     required this.price,
     required this.imagePath,
-    required this.description,
-    this.quantity = 1,
+    
     }
   );
 }
