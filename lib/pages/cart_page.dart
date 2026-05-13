deb60b23781d2883cbcc6adb88fe82130f5a1a62
Mubarek Jemal <mubarekj71@gmail.com>
2026-05-14 00:00:12 +0300

    fix

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -16,6 +16,7 @@ class CartPage extends StatelessWidget {
         children: [
           Text('My Cart', style: TextStyle(
             fontSize: 24,
+            fontWeight: FontWeight.bold
           ),),
           SizedBox(height: 10,),
 
